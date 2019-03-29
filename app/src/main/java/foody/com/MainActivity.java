305336d111bf91cbd565794c079fcc507087f31a
Souksavanh <souksavanh2504@gmail.com>
2019-03-29 19:50:57 +0700

    Display data on database

--- app/src/main/java/foody/com/MainActivity.java
+++ app/src/main/java/foody/com/MainActivity.java
@@ -2,6 +2,7 @@ package foody.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,26 +14,28 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private EditText txtsearch;
     private Button btnsearch;
-    private ListView dataView;
     private MySQLConnect mySQLConnect;
+    private ListView ListQuan;
     private List<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
         update();
     }
+
     public void update(){
         items = mySQLConnect.getData();
-        dataView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+        ListQuan.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
     }
 
-    public void init() {
+    public void init(){
         txtsearch = findViewById(R.id.txtsearch);
         btnsearch = findViewById(R.id.btnsearch);
-        dataView = findViewById(R.id.dataView);
+        ListQuan = findViewById(R.id.ListQuan);
         mySQLConnect = new MySQLConnect(MainActivity.this);
     }
 }
--- app/src/main/java/foody/com/MySQLConnect.java
+++ app/src/main/java/foody/com/MySQLConnect.java
@@ -1,6 +1,7 @@
 package foody.com;
 
 import android.app.Activity;
+import android.os.StrictMode;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -9,6 +10,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,50 +27,54 @@ public class MySQLConnect {
 
     private final Activity main;
     private List<String> list;
-    public String URL = "http://157.230.42.137/", GET_URL="android/get_quan.php";
+    public String URL = "http://157.230.42.137/", GET_URL = "android/get_quan.php";
 
     public MySQLConnect(){
         main = null;
-
     }
+
     public MySQLConnect(Activity mainA){
         main = mainA;
         list = new ArrayList<String>();
     }
-    public List<String> getData(){
+
+    public List<String> getData() {
+
         String url = URL + GET_URL;
+
         StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                showJson(response);
-                Toast.makeText(main, list.get(0), Toast.LENGTH_LONG).show();
+                showJSON(response);
+                Toast.makeText(main, list.get(0),Toast.LENGTH_LONG).show();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(main, error.getMessage().toString(), Toast.LENGTH_LONG).show();
-            }
-        }
-        );
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(main,error.getMessage().toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
         RequestQueue requestQueue = Volley.newRequestQueue(main.getApplicationContext());
         requestQueue.add(stringRequest);
 
         return list;
     }
-    private void showJson(String response){
-        String ten_quan = "";
 
+    private void showJSON(String response) {
+        String ten_quan = "";
         try {
+
             JSONObject jsonObject = new JSONObject(response);
             JSONArray result = jsonObject.getJSONArray("result");
 
-            for (int i = 0; i < result.length(); i++){
-                JSONObject collectData = result.getJSONObject(i);
-                ten_quan = collectData.getString("ten_quan");
+            for (int i = 0; i < result.length(); i++) {
+                JSONObject collegeData = result.getJSONObject(i);
+                ten_quan = collegeData.getString("ten_quan");
                 list.add(ten_quan);
             }
-        }catch (JSONException ex){
-            ex.printStackTrace();
-        }
+
+        } catch (JSONException e) {e.printStackTrace();}
     }
 }
