176a198dca9c2bcc9b799158a6292da7a86b2481
mikasaloliz <11161502106@hu.edu.vn>
2019-03-29 22:55:27 +0700

    notification

--- app/src/main/java/foody/com/MainActivity.java
+++ app/src/main/java/foody/com/MainActivity.java
@@ -1,5 +1,8 @@
 package foody.com;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,28 +14,31 @@ import android.widget.GridView;
 import android.widget.ListView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txtadd;
     private Button btnadd;
+    private Button bt1;
+    private Button bt2;
     private MySQLConnect mySQLConnect;
     private ListView ListQuan;
     private List<String> items;
     private ArrayAdapter<String> adt;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference().child("test");
 
-        myRef.setValue("Hello, World!");
 
         init();
         update();
@@ -44,8 +50,41 @@ public class MainActivity extends AppCompatActivity {
                 items.add(txtadd.getText().toString());
                 adt.notifyDataSetChanged();
                 txtadd.setText("");
+
+
             }
         });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference().child("1");
+                myRef.setValue("1");
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference().child("1");
+                myRef.setValue("2");
+            }
+        });
+        mRootRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                NotificationManager notif=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notify=new Notification.Builder(getApplicationContext()).setContentTitle("test").setSmallIcon(R.drawable.ic_launcher_foreground).build();
+                notify.flags |= Notification.FLAG_AUTO_CANCEL;
+                notif.notify(0,notify);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+
     }
 
     public void update(){
@@ -58,6 +97,8 @@ public class MainActivity extends AppCompatActivity {
     public void init(){
         txtadd = findViewById(R.id.txtadd);
         btnadd = findViewById(R.id.btnadd);
+        bt1 = findViewById(R.id.button2);
+        bt2 = findViewById(R.id.button3);
         ListQuan = findViewById(R.id.ListQuan);
         mySQLConnect = new MySQLConnect(MainActivity.this);
     }
